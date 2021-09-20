32d91cee5892e54ebfbac7ef47641e4e4881e7a5
RVLareu <rlareu@fi.uba.ar>
2021-09-20 14:53:16 -0300

    primes funcionando hasta 16k, lekea 1 file descriptor que van heredando

--- labPrimes/primes.c
+++ labPrimes/primes.c
@@ -1,49 +1,107 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/wait.h>
+
+
+void leerFiltrar(int *fd);
 
 int main(int argc, char* argcv[]) {
-
-
+    /////////// Leo el número a calcular //////////////
     int n =atoi(argcv[1]);
-    int primo = 2;
-    int arrn[n];
-    for(int i = 0; i < n-1; i++){
-        arrn[i] = i+2;
-        printf("ARREGLO DE NATURALES: %d\n", arrn[i]);
-    }
 
+    /////////// creo pipe a derecha //////////////
     int fds[2];
     int r = pipe(fds);
+    if (r == -1) {
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
 
-    r = write(fds[1], &arrn[0], sizeof(arrn));
-
-    
-    
-    int i = fork();
-    
-
-
-    if (i == 0) {
-
-        int recibido[n];
-        int err = read(fds[0], &recibido[0], sizeof(recibido));
-
-        for(int i = 0; i < n-1; i++){
-            if (recibido[i] % primo == 0) {
-                printf("%d\n", recibido[i]);
-            }
+    /////////// Coloco arreglo de numeros naturales en pipe //////////////   
+    for(int i = 2; i <= n; i++) {
+        r = write(fds[1], &i, sizeof(i));
+        if (r < 0) {
+            perror("Error en write");
+            _exit(-1);
         }
-
-        
-
-
-
-
-    } else {
-
     }
 
+    /////////// termino escritura y cierro //////////////   
+    close(fds[1]);
 
+
+    /////////// Leo y filtro en el siguiente proceso //////////////   
+
+    leerFiltrar(&fds[0]);
+
+
+    return 0;
+
+
+}
+
+
+void leerFiltrar(int *fd) {
+      
+    int fds[2];
+    int r = pipe(fds);
+    
+    /////////// Creo proceso hijo ////////////// 
+    int i = fork();
+	if (i < 0) {
+		printf("Error en fork! %d\n", i);
+		_exit(-1);
+
+	}   
+
+    if (i == 0) {
+        /////////// PROCESO HIJO //////////////   
+
+
+        //valor a leer del pipe
+        int siguiente;
+        //primo       
+        int p;
+
+        /////////// Leo del pipe y extraigo el primo //////////////   
+        int err = read(*fd, &p, sizeof(p));
+        if (err == 0){
+            _exit(0);
+        }
+        printf("%d\n", p);  
+
+        /////////// Mientras el pipe siga abierto //////////////   
+        while(err != 0) {
+            err = read(*fd, &siguiente, sizeof(siguiente));
+			if (err < 0) {
+				perror("Error en read");
+				_exit(-1);
+			}
+            
+            if(siguiente % p != 0){
+                 
+                r = write(fds[1], &siguiente, sizeof(siguiente));
+                if (r < 0) {
+                    perror("Error en write");
+                    _exit(-1);
+                }
+            }
+        }
+        /////////// cierro el de lectura que me llamó, cierro por el que escribí//////////////  
+        close(fds[1]);
+        close(*fd);
+        /////////// llamo recursivamente con la direccion del file descriptor de lectura //////////////   
+        leerFiltrar(&fds[0]);
+        _exit(0);
+      
+        
+    } else {
+        close(*fd);
+        close(fds[0]);
+        close(fds[1]);
+        /////////// PROCESO PADRE espera //////////////   
+        wait(NULL);
+    }
+    _exit(0);
 }
